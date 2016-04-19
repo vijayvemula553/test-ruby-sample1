b32ed70b0a9d5da9922d838762f68bce61602985
Weiwei Zheng <weiwz@amazon.com>
2016-04-18 17:19:04 -0700

    changed HelloWorld_spec.rb and yaml file

--- HelloWorld_spec.rb
+++ HelloWorld_spec.rb
@@ -5,7 +5,7 @@ describe HelloWorld do
       it "The say_hello method should return 'Hello World'" do 
          hw = HelloWorld.new 
          message = hw.say_hello 
-         expect(message).to eq "Hello, world!" 
+         puts 'Succeed' if expect(message).to eq "Hello, world!" 
       end
       
    end 
